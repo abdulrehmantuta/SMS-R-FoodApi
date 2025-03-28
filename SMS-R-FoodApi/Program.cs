3dfa8e0b0148a931a66a2c280703364e2ea519b3
abdulrehmantut <abdulrehmantutal@gmail.com>
2025-03-28 11:44:37 -0700

    New Commit github

--- SMS-R-FoodApi/Program.cs
+++ SMS-R-FoodApi/Program.cs
@@ -1,3 +1,55 @@
+//using Microsoft.EntityFrameworkCore;
+//using SMS_R_FoodApi.Data;
+//using SMS_R_FoodApi.Repository.IRepository;
+//using SMS_R_FoodApi.Repository;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//// CORS Policy Name
+//var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+//// Add services to the container.
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//builder.Services.AddScoped<IItemCategoryRepository, ItemCategoryRepository>();
+//builder.Services.AddScoped<IItemRepository, ItemRepository>();
+//builder.Services.AddScoped<ISaleRepository, SaleRepository>();
+//builder.Services.AddControllers();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//// ?? Add CORS Policy
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy(name: MyAllowSpecificOrigins,
+//        policy =>
+//        {
+//            policy.WithOrigins("http://localhost:4200") // Angular App Ka Origin
+//                  .AllowAnyMethod()
+//                  .AllowAnyHeader()
+//                  .AllowCredentials();
+//        });
+//});
+
+//var app = builder.Build();
+
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+//// ?? Use CORS Middleware (Important)
+//app.UseCors(MyAllowSpecificOrigins);
+
+//app.UseAuthorization();
+//app.MapControllers();
+//app.Run();
+
+
 using Microsoft.EntityFrameworkCore;
 using SMS_R_FoodApi.Data;
 using SMS_R_FoodApi.Repository.IRepository;
@@ -19,17 +71,19 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// ?? Add CORS Policy
+// ? Add CORS Policy (Updated)
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-        policy =>
-        {
-            policy.WithOrigins("http://localhost:4200") // Angular App Ka Origin
-                  .AllowAnyMethod()
-                  .AllowAnyHeader()
-                  .AllowCredentials();
-        });
+    options.AddPolicy(MyAllowSpecificOrigins, policy =>
+    {
+        policy.WithOrigins(
+            "http://localhost:4200",  // Local Development
+            "https://abdulrehmantuta.github.io"  // GitHub Pages Frontend
+        )
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials();
+    });
 });
 
 var app = builder.Build();
@@ -42,7 +96,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// ?? Use CORS Middleware (Important)
+// ? CORS Middleware Enable (Fix)
 app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
